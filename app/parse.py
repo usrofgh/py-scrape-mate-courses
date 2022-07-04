967524eacea92e4f9778824c250e033e976626bc
danylott <d.tuytuyshkin@gmail.com>
2022-07-04 16:12:11 +0300

    Task description

---
+++ app/parse.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class CourseType(Enum):
+    FULL_TIME = "full-time"
+    PART_TIME = "part-time"
+
+
+@dataclass
+class Course:
+    name: str
+    short_description: str
+    type: CourseType
+
+
+def get_all_courses() -> list[Course]:
+    pass
