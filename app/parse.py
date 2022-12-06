c56eebd45ecf48a2a1fd35b3f4247b573b4f277d
test <test@gmail.com>
2022-12-06 18:56:31 +0200

    get_details_info_of_course function was added and some renamed

--- app/parse.py
+++ app/parse.py
@@ -17,40 +17,53 @@ class Course:
     short_description: str
     modules: int
     topics: int
-    duration = None
+    duration: int | None
     course_type: CourseType
 
 
 URL_MATE_BASE = "https://mate.academy/"
 
 
-def get_single_course_info(course_common_info: BeautifulSoup, time_type: CourseType) -> Course:
+def get_details_info_of_course(link: str, time_type: CourseType) -> tuple:
+    detailed_page = requests.get(link).content
+    detailed_page_soup = BeautifulSoup(detailed_page, "html.parser")
+    detailed_info = detailed_page_soup.select("p.CourseModulesHeading_text__EdrEk")
+    modules, topics = [name.text.split()[0] for name in detailed_info[:2]]
+
+    if time_type.value == time_type.FULL_TIME.value:
+        duration = detailed_info[-1].text.split()[0]
+    else:
+        duration = None
+
+    return modules, topics, duration
+
+
+def get_info_about_course(course_common_info: BeautifulSoup, time_type: CourseType) -> Course:
+
     link_to_details_info = urljoin(URL_MATE_BASE,
                                    course_common_info.select_one("a.mb-16[href]")["href"])
 
-    detailed_page = requests.get(link_to_details_info).content
-    detailed_page_soup = BeautifulSoup(detailed_page, "html.parser")
+    modules, topics, duration = get_details_info_of_course(link_to_details_info, time_type)
 
-    course = Course(
+    return Course(
         name=course_common_info.select_one(".typography_landingH3__vTjok").text,
         short_description=course_common_info.select_one(".CourseCard_courseDescription__Unsqj").text,
-        modules=int(detailed_page_soup.select("p.CourseModulesHeading_text__EdrEk")[0].text.split()[0]),
-        topics=int(detailed_page_soup.select("p.CourseModulesHeading_text__EdrEk")[1].text.split()[0]),
-        course_type=time_type,
+        modules=modules,
+        topics=topics,
+        duration=duration,
+        course_type=time_type
     )
-    if time_type.value == time_type.FULL_TIME.value:
-        course.duration = int(detailed_page_soup.select("p.CourseModulesHeading_text__EdrEk")[2].text.split()[0])
-    return course
 
 
 def get_courses(soup_page: BeautifulSoup, time_type: CourseType) -> [Course]:
     course_cards = soup_page.select(f"#{time_type.value} .CourseCard_cardContainer__7_4lK")
-    return [get_single_course_info(course, time_type) for course in course_cards]
+    return [get_info_about_course(course, time_type) for course in course_cards]
 
 
 def get_all_courses() -> list[Course]:
-    page = requests.get(URL_MATE_BASE).content
-    mate_base_page_soup = BeautifulSoup(page, "html.parser")
+    mate_base_page = requests.get(URL_MATE_BASE).content
+    mate_base_page_soup = BeautifulSoup(mate_base_page, "html.parser")
+
     full_time = get_courses(mate_base_page_soup, CourseType.FULL_TIME)
     part_time = get_courses(mate_base_page_soup, CourseType.PART_TIME)
 
@@ -58,7 +71,7 @@ def get_all_courses() -> list[Course]:
 
 
 def main() -> None:
-    print(get_all_courses())
+    get_all_courses()
 
 
 if __name__ == "__main__":
